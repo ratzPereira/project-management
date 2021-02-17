64a64bdeffd822adddce945f9e17e24fc74d4cf2
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-02-17 17:57:57 -0100

    Setting up configs for integration tests, added h2 db configs to work in integration tests. added test for  save employee

---
+++ src/test/java/com/ratz/pma/dao/ProjectRepositoryIntegrationTest.java
@@ -0,0 +1,37 @@
+package com.ratz.pma.dao;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+import com.ratz.pma.entities.Project;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@SqlGroup({ @Sql(executionPhase= ExecutionPhase.BEFORE_TEST_METHOD, scripts= {"classpath:schema.sql", "classpath:data.sql"}),
+	@Sql(executionPhase= ExecutionPhase.AFTER_TEST_METHOD, scripts= "classpath:drop.sql")})
+
+	class ProjectRepositoryIntegrationTest {
+
+	@Autowired
+	ProjectRepository proRepo;
+	
+	@Test
+	 void ifNewProjectSaved_thenSuccess() {
+		Project newProject = new Project("new test", "COMPLETE", "Test Description");
+		proRepo.save(newProject);
+		
+		assertEquals(5, proRepo.findAll().size());
+	}
+}
