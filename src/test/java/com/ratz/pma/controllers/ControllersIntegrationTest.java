c4e08159d08fd07ee581296bd8ff7b36adea9e33
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2021-02-17 22:08:43 -0100

    Added integration tests for controllers/views

---
+++ src/test/java/com/ratz/pma/controllers/ControllersIntegrationTest.java
@@ -0,0 +1,33 @@
+package com.ratz.pma.controllers;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class ControllersIntegrationTest {
+
+	@LocalServerPort
+	private int port;
+	
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	@Test
+	void homePageReturnVersionNumberCorrectly_thenSucess() {
+		String renderedHtml = this.restTemplate.getForObject("http://localhost:" + port +"/", String.class);
+		
+		assertEquals(renderedHtml.contains("0.0.1"),false);
+		
+		
+		
+	}
+}
